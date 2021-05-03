c09f6420fa4166faffb03acadab691fb82dfb3fd
JuanVascoGiraldo <79484258+JuanVascoGiraldo@users.noreply.github.com>
2021-05-03 04:52:20 -0500

    añadir modulo de rutina y agregar validación de datos

--- js/validar.js
+++ js/validar.js
@@ -94,8 +94,8 @@ function añadirdieta(){
     var cmañana= document.adieta.Mañana.value;
     var ctarde= document.adieta.Tarde.value;
     var cnoche= document.adieta.Noche.value;
-
     var todo = nombre+clasificacion;
+    var seguir= true;
     for(var i=0; i<todo.length; i++){
         var caracter = todo.charAt(i);
         for(var j = 0; j < comprobar.length; j++){
@@ -135,9 +135,103 @@ function añadirdieta(){
     if(seguir == false){
         alert("se han introducido caracteres no validos");
     }else{
-        alert("ha ingresado al sistema");
+        alert("Se ha guardado la dieta");
     }
+}
 
+function modificareje(){
+    var select = document.getElementById("Parametro");
+    var selectedOperation = select.options[select.selectedIndex].value;
+    var cambio = document.Mejercicio.cambio.value;
+    var seguir= true;
 
+    if(selectedOperation === "nombre"){
+        for(var i=0; i<cambio.length; i++){
+            var caracter = cambio.charAt(i);
+            for(var j = 0; j < comprobar3.length; j++){
+                if(caracter == comprobar3.charAt(j))
+                    break;
+                if(j == comprobar3.length-1){
+                    seguir = false;
+                    break;
+                }
+            }
+        }
+        if(seguir == false){
+            alert("se han introducido caracteres no validos para el nombre");
+        }else{
+            alert("Se ha cambiado el nombre");
+        }
+    }
+    if(selectedOperation === "clasificacion"){
+        seguir= false;
+        if(cambio === "baja" || cambio === "media" || cambio === "alta"){
+            seguir=true;
+        }
 
+        if(seguir == false){
+            alert("Para el parametro de Clasificación solo se permite las opciones baja, media y alta");
+        }else{
+            alert("Se ha cambiado la clasificacion");
+        }
+        
+    }
+    if(selectedOperation === "calorias"){
+        for(var i=0; i<cambio.length; i++){
+            var caracter = cambio.charAt(i);
+            for(var j = 0; j < comprobar2.length; j++){
+                if(caracter == comprobar2.charAt(j))
+                    break;
+                if(j == comprobar2.length-1){
+                    seguir = false;
+                    break;
+                }
+            }
+        }
+        if(seguir == false){
+            alert("se han introducido caracteres no validos para las calorias");
+        }else{
+            alert("Se ha cambiado el nombre");
+        }
+    }
+}
+
+function modificardie(){
+    var select = document.getElementById("Parametro");
+    var selectedOperation = select.options[select.selectedIndex].value;
+    var cambio = document.Mdie.cambio.value;
+    var seguir= true;
+
+    if(selectedOperation === "nombre" || selectedOperation === "clasificacion" || selectedOperation === "descripcion"){
+        for(var i=0; i<cambio.length; i++){
+            var caracter = cambio.charAt(i);
+            for(var j = 0; j < comprobar.length; j++){
+                if(caracter == comprobar.charAt(j))
+                    break;
+                if(j == comprobar.length-1){
+                    seguir = false;
+                    break;
+                }
+            }
+        }
+    }
+    if(selectedOperation === "mañana"||selectedOperation === "tarde"||selectedOperation === "noche"){
+        for(var i=0; i<cambio.length; i++){
+            var caracter = cambio.charAt(i);
+            for(var j = 0; j < comprobar3.length; j++){
+                if(caracter == comprobar3.charAt(j))
+                    break;
+                if(j == comprobar3.length-1){
+                    seguir = false;
+                    break;
+                }
+            }
+        }
+        
+    }
+    if(seguir == false){
+            alert("se han introducido caracteres no validos");
+        }else{
+            alert("Se ha cambiado el nombre");
+        }
 }
